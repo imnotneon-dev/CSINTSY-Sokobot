d8176fe326e4527fc4280933c005cea6a450f8ae
Jenrick Lim <jenrick_lim@dlsu.edu.ph>
2025-09-26 17:30:28 +0800

    new feature: add a for-loop to find initial position of player            : added the brute force solution of TESTLEVEL

--- sokobot/src/solver/SokoBot.java
+++ sokobot/src/solver/SokoBot.java
@@ -11,12 +11,77 @@ public class SokoBot {
      * sequence
      * that just moves left and right repeatedly.
      */
-    try {
-      Thread.sleep(3000);
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
-    return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
-  }
 
+    // COMMENTED OUT (IDK IF WE NEED THIS)
+    // try {
+    //     Thread.sleep(3000);
+    // } catch (Exception ex) {
+    //     ex.printStackTrace();
+    // }
+
+    StringBuilder moves = new StringBuilder(); // To add in the move list, just do -- moves.append('Letter'); --
+                                               // NOTE: MOVES MUST BE IN small letters [u, d, l, r]
+    int playerX = -1, playerY = -1, found = 0;
+
+    /* To find the player's starting index (x, y) 
+       x = column
+       y = row
+
+          x
+        y # 0 1 2 
+          0 
+          1  '@'
+          2
+    */
+   
+    for (int y = 0; y < height && found == 0; y++) { 
+        for (int x = 0; x < width; x++) {
+            if (itemsData[y][x] == '@') {
+                playerX = x;
+                playerY = y;
+                found = 1;
+            }
+        }
+    }
+
+    moves.append('r');
+    moves.append('r');
+    moves.append('r');
+    moves.append('d');
+    moves.append('d');
+    moves.append('l');
+    moves.append('l');
+    moves.append('l');
+    moves.append('r');
+    moves.append('r');
+    moves.append('r');    
+    moves.append('u');
+    moves.append('u');
+    moves.append('l');
+    moves.append('l');
+    moves.append('l');
+    moves.append('d');
+    moves.append('u');
+    moves.append('r');
+    moves.append('r');
+    moves.append('r');
+    moves.append('d');
+    moves.append('d');
+    moves.append('l');
+    moves.append('l');
+    moves.append('u');
+    moves.append('d');
+    moves.append('r');
+    moves.append('r'); 
+    moves.append('u');
+    moves.append('u');
+    moves.append('l');
+    moves.append('d');
+    moves.append('l');
+    moves.append('l');
+    moves.append('u');
+    moves.append('r');
+
+    return moves.toString(); // Returns the moves
+  }
 }
